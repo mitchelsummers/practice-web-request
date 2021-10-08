0a2c4f079a6d259611e5c404869d9f41408dc862
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-08 13:08:26 -0600

    Adds loop Dictionary

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,9 +1,14 @@
 require "http"
 system "clear"
-puts "Enter a word for the definition"
-word = gets.chomp
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=API-KEY")
-definition = response.parse(:json)
-pp definition
-p definition[1]["text"]
-# p definition[0]["textProns"]
+word = 0
+if word != "q"
+  puts "Enter a word for the definition"
+  word = gets.chomp
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=API-KEY")
+  definition = response.parse(:json)
+  pp definition
+  p definition[1]["text"]
+  # p definition[0]["textProns"]
+else
+  break
+end
